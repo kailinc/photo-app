469cf2faef46b48c4137edb42ecd4053c4434ce6
kailinc <kailinc@bu.edu>
2017-07-16 14:08:02 -0400

    Set Up User Authentication with Devise -added login, sign up when someone visits site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  # to access the app you need to be a logged in user
+  # creates login for user_name, password, remember me, sign up, forgot password?
 end
