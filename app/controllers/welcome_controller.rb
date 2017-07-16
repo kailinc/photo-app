88eb50036e6566a7197eb8243154f86280531cfb
kailinc <kailinc@bu.edu>
2017-07-16 15:13:07 -0400

    Used Devise and Bootstrap to make Views -was facing an issue with favicon because the gems didn't download it -removed the html that required favicon -starting to love devise and bootstrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  # skip the before action on all controllers when you visit the page
   def index
   end
 end
